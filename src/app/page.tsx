a72d7fe8fd40aa79d601ad64c0cdc2e379bab162
maridivi <maridivi89@gmail.com>
2023-07-26 17:45:26 +0100

    Add lofi cafe card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import About from "@/components/About";
+import LofiCafe from "@/components/LofiCafe";
 import Grid from "@/components/ui/Grid";
 
 export default function Home() {
   return (
     <Grid>
       <About />
+      <LofiCafe />
     </Grid>
   );
 }
