a225f63b85a12996fa33298b9fa64451ea7e0818
maridivi <maridivi89@gmail.com>
2023-08-10 15:45:35 +0200

    Edit about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,15 +1,40 @@
+import Image from "next/image";
 import H1 from "./ui/H1";
+import Stack from "./ui/Stack";
+import { AiOutlineTwitter, AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import Link from "next/link";
+import CustomLink from "./ui/CustomLink";
 
 export default function About() {
   return (
-    <H1>
-      Hi there! I'm <strong>Marianna</strong>, a self-taught web developer with
-      a love for building nice apps and websites.<br></br>
-      <br></br>
-      In my free time, you'll find me in the gym, lifting weights and striving
-      for personal growth both physically and mentally.<br></br>
-      <br></br>I also enjoy reading historical fiction, as well as watching high
-      quality TV shows and movies.
-    </H1>
+    <Stack className="flex items-center">
+      <Image
+        src="/images/avatar.jpeg"
+        height={75}
+        width={130}
+        className="rounded-full"
+        alt="avatar"
+      />
+      <Stack vertical>
+        <H1>
+          Hi there! I'm <strong>Marianna</strong>, a self-taught web developer
+          with a love for building nice apps and websites.
+        </H1>
+        <Stack>
+          <CustomLink href="">
+            <AiOutlineTwitter size={24} />
+            Twitter
+          </CustomLink>
+          <CustomLink href="">
+            <AiFillGithub size={24} />
+            Github
+          </CustomLink>
+          <CustomLink href="">
+            <AiFillLinkedin size={24} />
+            Linkedin
+          </CustomLink>
+        </Stack>
+      </Stack>
+    </Stack>
   );
 }
