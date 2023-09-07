28421f7e818e104b15746bcbb7e97c5ad41a332d
maridivi <maridivi89@gmail.com>
2023-09-07 15:35:52 +0100

    Edit footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,3 @@
-import Link from "next/link";
-import Stack from "./ui/Stack";
-
 export default function Footer() {
   const getYear = () => {
     const date = new Date();
@@ -11,15 +8,34 @@ export default function Footer() {
   return (
     <div className="flex justify-center max-w-xl w-full mx-auto bottom-0 min-h-0 overflow-hidden">
       <h5 className="text-xs text-gray-500">
-        © {getYear()} Created by
+        I've built this website with
         <a
-          href="https://mariannadivito.com"
+          href="https://nextjs.org/"
           target="_blank"
           className="text-violet-500"
         >
           {" "}
-          Marianna Di Vito
+          Next.js
         </a>
+        . Deployed on
+        <a
+          href="https://vercel.com/"
+          target="_blank"
+          className="text-violet-500"
+        >
+          {" "}
+          Vercel
+        </a>
+        . View
+        <a
+          href="https://github.com/maridivi/new-personal-portfolio"
+          target="_blank"
+          className="text-violet-500"
+        >
+          {" "}
+          source
+        </a>
+        .
       </h5>
     </div>
   );
