0537880174639a709a2f255effd208b8c66d1cce
maridivi <maridivi89@gmail.com>
2023-09-15 15:48:52 +0100

    Attempt to add og image

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -6,18 +6,19 @@ export default function Head() {
       <meta name="description" content="Front-end web developer." />
       <meta property="og:url" content="https://lofi.cafe" />
       <meta property="og:type" content="website" />
-      <meta property="og:image" content="https://lofi.cafe/card.jpg" />
+
+      <meta property="og:image" content="<generated>" />
+      <meta property="og:image:type" content="<generated>" />
+      <meta property="og:image:width" content="<generated>" />
+      <meta property="og:image:height" content="<generated>" />
       <meta name="twitter:title" content={"Marianna Di Vito"} />
       <meta
         name="twitter:description"
         content={"I'm a front-end web developer."}
       />
-      <meta
-        name="twitter:image"
-        content={"https://fabrizio.so/apple-touch-icon-precomposed.jpeg"}
-      />
+
       <meta name="twitter:card" content={"summary_large_image"} />
-      <meta property="og:image" content="https://mariannadivito.com/api/og" />
+
       <meta name="twitter:creator" content="Marianna Di Vito" />
       <link rel="icon" href="/favicon.ico" />
     </>
